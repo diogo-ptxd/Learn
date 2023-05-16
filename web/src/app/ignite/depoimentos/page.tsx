acfde0e03d43d4312c8b86b3841b5588ec2e24e6
diogo-ptxd <diogo-inacio2005@hotmail.com>
2023-05-16 12:14:56 +0000

    Initial commit

---
+++ web/src/app/ignite/depoimentos/page.tsx
@@ -0,0 +1,5 @@
+export default function Depos(){
+  return(
+    <h1>Depoimentos</h1>
+  )
+}
---
+++ web/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/Components/Button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <h1>
+      Hello World!
+    <Button title="Diogo"/>
+    <Button title="Nelson"/>
+    <Button title="Simao Gay"/>
+    </h1>
+  )
+}
